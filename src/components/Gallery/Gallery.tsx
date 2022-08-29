674d44dc3ebec3b18c39a20a78f7889f816810e8
Suraj Pheudin <pheudinsuraj@gmail.com>
2022-08-29 14:41:04 +0545

    feat: complete question 3

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -4,15 +4,23 @@ import "./gallery.css";
 const Gallery = () => {
   return (
     <div className="root">
-      <Card height="100px" text="Widget 1"></Card>
-      <Card height="200px" text="Widget 2"></Card>
-      <Card height="150px" text="Widget 3" isSpecial></Card>
+      <div>
+        <div>
+          <Card height="130px" text="Widget 1"></Card>
+          <Card height="200px" text="Widget 4"></Card>
+        </div>
+        <div>
+          <Card height="200px" text="Widget 2"></Card>
+          <Card height="50px" text="Widget 5"></Card>
+        </div>
+      </div>
 
-      <Card height="200px" text="Widget 4"></Card>
-
-      <Card height="50px" text="Widget 5"></Card>
-
-      <Card height="100px" text="Widget 6" isSpecial></Card>
+      <div>
+        <Card height="150px" text="Widget 3" isSpecial></Card>
+        <div className="minus_margin_top">
+          <Card height="130px" text="Widget 6" isSpecial></Card>
+        </div>
+      </div>
     </div>
   );
 };
