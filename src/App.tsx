ca5a202d7e810fe6e6032703ce45a23ffff973c3
Suraj Pheudin <pheudinsuraj@gmail.com>
2022-08-29 14:07:59 +0545

    feat: complete the answer of question 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        margin: "100px",
+      }}
+    >
+      <p>Answer: Differences between SSR and SSG:</p>
+      <ol>
+        <li>In SSR, the page is generated on server during each request.</li>
+        <li>In SSG, the page is generated on server during the build time.</li>
+      </ol>
+    </div>
+  );
 }
 
 export default App;
